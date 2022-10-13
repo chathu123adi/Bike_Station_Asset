55fa8305ddab1e5c7ad36ef1d5b4b8637e04c838
Chathuranga Adikari <chathuranga.adikari@gmail.com>
2022-10-13 14:18:08 +0530

    Initial Commit

---
+++ Bike_Station_Asset/Bike_Station_AssetApp.swift
@@ -0,0 +1,17 @@
+//
+//  Bike_Station_AssetApp.swift
+//  Bike_Station_Asset
+//
+//  Created by vinoj randika on 2022-10-13.
+//
+
+import SwiftUI
+
+@main
+struct Bike_Station_AssetApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
